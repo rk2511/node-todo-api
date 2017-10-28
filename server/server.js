d12a32eaa138a5e97ae7eaae2cb9f47594cc9ff1
vikki <radhakrishnan1125@gmail.com>
2017-10-28 23:00:12 +0530

    added id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {user} = require('./models/user.js');
@@ -26,8 +26,23 @@ Todo.find().then((todos) => {
 res.send({todos});
 }, (err) => {
 res.status(400).send(err);
+  });
 });
 
+app.get('/todos/:id', (req,res) => {
+var id=req.params.id;
+  if(!ObjectID.isValid(id)) {
+  return res.status(404).send('Invalid ID');
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send('Not found');
+    }
+    res.send({todo});
+  }, (err) => {
+    res.status(400).send('Bad request');
+  })
 });
 
 app.listen(3000, () => {
