68ede38ac9004a4dc1f68fdf82c57dab96026751
vikki <radhakrishnan1125@gmail.com>
2017-11-06 00:31:44 +0530

    added delete test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -264,3 +264,23 @@ describe('test for login', () => {
 });
 
 });
+
+describe('Delete or logout a user', () => {
+  it('should get the user logout', (done) => {
+    request(app)
+    .delete('/users/me/token')
+    .set('x-auth', users[0].tokens[0].token)
+    .expect(200)
+    .end((err,res) => {
+      if(err) {
+        return done(err);
+      }
+      User.findById(users[0]._id).then((user) => {
+        expect(user.tokens.length).toBe(0);
+        done();
+      }).catch((err) => {
+        done(err);
+      });
+    });
+  });
+});
